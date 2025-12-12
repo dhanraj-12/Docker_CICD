d080bf839a64ce6ab773a3ce63f497fed6ca0282
dhanraj-12 <dj004786@gmail.com>
2025-12-12 18:09:48 +0530

    first commit

---
+++ Backend/dist/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import mongoose from "mongoose";
+import { User } from "./models/user.js";
+import cors from 'cors';
+const app = express();
+app.use(express.json()); // VERY IMPORTANT
+app.use(cors());
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect("mongodb://mongodb:27017/testdb");
+//         console.log("Connected to MongoDB");
+//     } catch (err) {
+//         console.error(err);
+//     }
+//     console.log("DB connection attempt finished");
+// };
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://dj004786:VgFZDNsjJTJvatDi@web-dev-practice.9tnbr.mongodb.net/DevopDb?retryWrites=true&w=majority&appName=Web-Dev-practice/devops");
+        console.log("Connected to MongoDB");
+    }
+    catch (err) {
+        console.error(err);
+    }
+    console.log("DB connection attempt finished");
+};
+connectDB();
+// GET all users
+app.get("/", async (req, res) => {
+    const data = await User.find();
+    res.json(data);
+    // res.send(data);
+});
+// Add a user
+app.post("/add-user", async (req, res) => {
+    const username = "Dhanraj";
+    const newUser = new User({ username });
+    await newUser.save();
+    res.json({ message: "User added", user: newUser });
+});
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
+//# sourceMappingURL=index.js.map
---
+++ client/src/App.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import axios from "axios";
+import { api } from "./config";
+
+function App() {
+  const [users, setUsers] = React.useState([]);
+
+  // Get all users
+  const handleGet = async () => {
+    try {
+      const res = await axios.get(`${api}`);
+      console.log(res.data);
+      setUsers(res.data); // <-- update UI
+    } catch (err) {
+      console.error("Error fetching users:", err);
+    }
+  };
+
+  // Add a new user
+  const handleAdd = async () => {
+    try {
+      const res = await axios.post(`${api}/add-user`);
+      console.log(res.data);
+
+      // refresh list after adding
+      handleGet();
+    } catch (err) {
+      console.error("Error adding user:", err);
+    }
+  };
+
+  // Run once on component mount
+  React.useEffect(() => {
+    handleGet();
+  }, []);
+
+  return (
+    <>
+      <button onClick={handleAdd}>Add User</button>
+      <button onClick={handleGet}>Get Users</button>
+
+      <h2>Users List:</h2>
+      <ul>
+        {users.length > 0 ? (
+          users.map((user, index) => (
+            <li key={index}>{JSON.stringify(user)}</li>
+          ))
+        ) : (
+          <p>No users found</p>
+        )}
+      </ul>
+    </>
+  );
+}
+
+export default App;
